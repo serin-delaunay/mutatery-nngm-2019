a063de14c0ef0ab5c1ecaf2b0779072a562d0108
Allison Parrish <allison@decontextualize.com>
2016-03-09 14:27:06 -0500

    initial commit still very incomplete but at least it works

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='tracery',
+    version='0.0.1',
+    description="Python port of Kate Compton's tracery text generation library",
+    long_description="TK",
+    author="Allison Parrish",
+    author_email='allison@decontextualize.com',
+    url='https://github.com/aparrish/tracery',
+    packages=[
+        'tracery',
+    ],
+    install_requires=[],
+    license="BSD",
+    zip_safe=True,
+    keywords='tracery',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'License :: OSI Approved :: BSD License',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Artistic Software",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+    test_suite='tests',
+    tests_require=[]
+)
