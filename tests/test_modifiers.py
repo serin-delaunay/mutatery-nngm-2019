d3c5d0be79594c27927b0d6a2fcc26fd6b8c8514
hugovk <hugovk@users.noreply.github.com>
2016-03-13 23:57:23 +0200

    Don't redefine input()

--- tests/test_modifiers.py
+++ tests/test_modifiers.py
@@ -21,150 +21,150 @@ class TestModifiers(unittest.TestCase):
 
     def test_replace(self):
         # Arrange
-        input = "a big ship"
+        text = "a big ship"
 
         # Act
-        output = modifiers.replace(input, "ship", "shop")
+        output = modifiers.replace(text, "ship", "shop")
 
         # Assert
         self.assertEqual(output, "a big shop")
 
     def test_capitalize_all(self):
         # Arrange
-        input = "abc def"
+        text = "abc def"
 
         # Act
-        output = modifiers.capitalizeAll(input)
+        output = modifiers.capitalizeAll(text)
 
         # Assert
         self.assertEqual(output, "Abc Def")
 
     def test_capitalize_(self):
         # Arrange
-        input = "abc def"
+        text = "abc def"
 
         # Act
-        output = modifiers.capitalize_(input)
+        output = modifiers.capitalize_(text)
 
         # Assert
         self.assertEqual(output, "Abc def")
 
     def test_a_consonant(self):
         # Arrange
-        input = "house"
+        text = "house"
 
         # Act
-        output = modifiers.a(input)
+        output = modifiers.a(text)
 
         # Assert
         self.assertEqual(output, "a house")
 
     def test_a_u_something_i(self):
         # Arrange
-        input = "unicorn"
+        text = "unicorn"
 
         # Act
-        output = modifiers.a(input)
+        output = modifiers.a(text)
 
         # Assert
         self.assertEqual(output, "a unicorn")
 
     def test_a_u_not_i(self):
         # Arrange
-        input = "underdog"
+        text = "underdog"
 
         # Act
-        output = modifiers.a(input)
+        output = modifiers.a(text)
 
         # Assert
         self.assertEqual(output, "an underdog")
 
     def test_a_vowel(self):
         # Arrange
-        input = "elephant"
+        text = "elephant"
 
         # Act
-        output = modifiers.a(input)
+        output = modifiers.a(text)
 
         # Assert
         self.assertEqual(output, "an elephant")
 
     def test_first_s(self):
         # Arrange
-        input = "elephant in a phonebox"
+        text = "elephant in a phonebox"
 
         # Act
-        output = modifiers.firstS(input)
+        output = modifiers.firstS(text)
 
         # Assert
         self.assertEqual(output, "elephants in a phonebox")
 
     def test_s_ends_in_x(self):
         # Arrange
-        input = "box"
+        text = "box"
 
         # Act
-        output = modifiers.s(input)
+        output = modifiers.s(text)
 
         # Assert
         self.assertEqual(output, "boxes")
 
     def test_s_ends_in_non_s(self):
         # Arrange
-        input = "goat"
+        text = "goat"
 
         # Act
-        output = modifiers.s(input)
+        output = modifiers.s(text)
 
         # Assert
         self.assertEqual(output, "goats")
 
     def test_s_ends_in_vowel_y(self):
         # Arrange
-        input = "monkey"
+        text = "monkey"
 
         # Act
-        output = modifiers.s(input)
+        output = modifiers.s(text)
 
         # Assert
         self.assertEqual(output, "monkeys")
 
     def test_s_ends_in_y_but_not_vowel_y(self):
         # Arrange
-        input = "telly"
+        text = "telly"
 
         # Act
-        output = modifiers.s(input)
+        output = modifiers.s(text)
 
         # Assert
         self.assertEqual(output, "tellies")
 
     def test_ed_ends_in_e(self):
         # Arrange
-        input = "glide"
+        text = "glide"
 
         # Act
-        output = modifiers.ed(input)
+        output = modifiers.ed(text)
 
         # Assert
         self.assertEqual(output, "glided")
 
     def test_ed_ends_in_y_but_not_vowel_y(self):
         # Arrange
-        input = "shimmy"
+        text = "shimmy"
 
         # Act
-        output = modifiers.ed(input)
+        output = modifiers.ed(text)
 
         # Assert
         self.assertEqual(output, "shimmied")
 
     def test_ed_ends_in_non_e_and_non_y(self):
         # Arrange
-        input = "jump"
+        text = "jump"
 
         # Act
-        output = modifiers.ed(input)
+        output = modifiers.ed(text)
 
         # Assert
         self.assertEqual(output, "jumped")
